dcf20f9ae4d1735b6e30341cf661728b86673dac
gagefulwood <gagetfulwood@gmail.com>
2024-07-07 13:06:31 -0500

    playlist controller and playist view

--- controllers/playlist_controller.py
+++ controllers/playlist_controller.py
@@ -1,2 +1,19 @@
+import tkinter as tk
+from views.playlist_view import PlaylistView
+
 class PlaylistController:
-    pass
+    def __init__(self, root):
+        self.root = root
+        self.view = PlaylistView(root, self)
+        self.name_var = tk.StringVar()
+        self.tracking_var = tk.DoubleVar()
+        self.clicking_var = tk.DoubleVar()
+        self.switching_var = tk.DoubleVar()
+        
+        self.view.name_entry.config(textvariable=self.name_var)
+        self.view.tracking_scale.config(variable=self.tracking_var)
+        self.view.tracking_entry.config(textvariable=self.tracking_var)
+        self.view.clicking_scale.config(variable=self.clicking_var)
+        self.view.clicking_entry.config(textvariable=self.clicking_var)
+        self.view.switching_scale.config(variable=self.switching_var)
+        self.view.switching_entry.config(textvariable=self.switching_var)
--- views/playlist_view.py
+++ views/playlist_view.py
@@ -1,10 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
 
-class PlaylistView(ttk.Frame):
+class PlaylistView(ttk.LabelFrame):
     def __init__(self, parent, controller):
-        super().__init__(parent)
+        super().__init__(parent, text="Playlist Maker")
         self.controller = controller
+        self.pack(expand=True, fill="both")
+        self.create_widgets()
+        
         
     def create_widgets(self):
-        self.playlist_listbox = tk.Listbox(self)
+        self.name = ttk.Label(self,text="Name:")
+        self.name.grid(row=0,column=0)
+        self.name_entry = ttk.Entry(self, width=30)
+        self.name_entry.grid(row=0,column=1)
+
+        self.tracking_label = ttk.Label(self,text="Tracking:")
+        self.tracking_label.grid(row=1,column=0)
+        self.tracking_scale = ttk.Scale(self, from_=0, to=100,
+                                        orient=tk.HORIZONTAL,
+                                        length=200)
+        self.tracking_scale.grid(row=1,column=1)
+        self.tracking_entry = ttk.Entry(self, width=4)
+        self.tracking_entry.grid(row=1,column=2)
+
+
+        self.clicking_label = ttk.Label(self,text="Clicking:")
+        self.clicking_label.grid(row=2,column=0)
+        self.clicking_scale = ttk.Scale(self, from_=0, to=100,
+                                        orient=tk.HORIZONTAL,
+                                        length=200)
+        self.clicking_scale.grid(row=2,column=1)
+        self.clicking_entry = ttk.Entry(self, width=4)
+        self.clicking_entry.grid(row=2,column=2)
+
+
+        self.switching_label = ttk.Label(self,text="Switching:")
+        self.switching_label.grid(row=3,column=0)
+        self.switching_scale = ttk.Scale(self, from_=0, to=100,
+                                        orient=tk.HORIZONTAL,
+                                        length=200)
+        self.switching_scale.grid(row=3,column=1)
+        self.switching_entry = ttk.Entry(self, width=4)
+        self.switching_entry.grid(row=3,column=2)
+        
+
+        self.generate_btn = ttk.Button(self, text="Generate")
+        self.generate_btn.grid(row=5,column=1)
+
+        self.scenario_listbox = tk.Listbox(self, width=65, height=9)
+        self.scenario_listbox.grid(row=6,column=0,columnspan=4)
